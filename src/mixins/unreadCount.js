84b82bd9a1bb0d8fc42eb0c2cfa6d169f68e9c3a
liuxiaofeng <13602490352@163.com>
2019-02-21 20:36:00 +0800

    switch env

--- src/mixins/unreadCount.js
+++ src/mixins/unreadCount.js
@@ -1,41 +1,41 @@
 import wepy from 'wepy'
 
 export default class unreadCount extends wepy.mixin {
-    data = {
+  data = {
         // 轮询
-        interval: null,
+    interval: null,
         // 未读消息数
-        unreadCount: 0
-    }
+    unreadCount: 0
+  }
     // 页面显示
-    onShow() {
-        this.updateUnreadCount()
-        this.interval = setInterval(() => {
-            this.updateUnreadCount()
-        }, 30000)
-    }
+  onShow() {
+    this.updateUnreadCount()
+    this.interval = setInterval(() => {
+      this.updateUnreadCount()
+    }, 30000)
+  }
     // 页面隐藏
-    onHide() {
+  onHide() {
         // 关闭轮询
-        clearInterval(this.interval)
-    }
+    clearInterval(this.interval)
+  }
     // 设置未读消息数
-    updateUnreadCount() {
+  updateUnreadCount() {
         // 从全局获取未读消息数
-        this.unreadCount = this.$parent.globalData.unreadCount
-        this.$apply()
+    this.unreadCount = this.$parent.globalData.unreadCount
+    this.$apply()
 
-        if (this.unreadCount) {
+    if (this.unreadCount) {
             // 设置 badge
-            wepy.setTabBarBadge({
-                index: 1,
-                text: this.unreadCount.toString()
-            })
-        } else {
+      wepy.setTabBarBadge({
+        index: 1,
+        text: this.unreadCount.toString()
+      })
+    } else {
             // 移除 badge
-            wepy.removeTabBarBadge({
-                index: 1
-            })
-        }
+      wepy.removeTabBarBadge({
+        index: 1
+      })
     }
+  }
 }
--- wepy.config.js
+++ wepy.config.js
@@ -41,6 +41,13 @@ module.exports = {
     }
   },
   plugins: {
+      replace: {
+          filter: /\.js$/,
+          config: {
+              find: /__BASE_URL__/g,
+              replace: prod ? "'https://larabbs.ergou.live/api'" : "'http://larabbs.test/api'"
+          }
+      }
   },
   appConfig: {
     noPromiseAPI: ['createSelectorQuery']
@@ -69,6 +76,13 @@ if (prod) {
           quality: 80
         }
       }
+    },
+    replace: {
+        filter: /\.js$/,
+        config: {
+            find: /__BASE_URL__/g,
+            replace: prod ? "'https://larabbs.ergou.live/api'" : "'http://larabbs.test/api'"
+        }
     }
   }
 }
