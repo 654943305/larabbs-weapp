59c676855e625e20e203c3ef900b7e020a1150b7
liuxiaofeng <13602490352@163.com>
2019-02-21 15:46:52 +0800

    reply delete

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -7,6 +7,8 @@ export default class ReplyMixin extends wepy.mixin {
         enablePullDownRefresh: true,
     }
     data = {
+        // 用户信息
+        user: null,
         // 回复数据
         replies: [],
         // 是否有更多数据
@@ -16,6 +18,51 @@ export default class ReplyMixin extends wepy.mixin {
         // 当前页数
         page: 1
     }
+    methods = {
+        // 删除回复
+        async deleteReply(topicId, replyId) {
+            // 确认是否删除
+            let res = await wepy.showModal({
+                title: '确认删除',
+                content: '您确认删除该回复吗',
+                confirmText: '删除',
+                cancelText: '取消'
+            })
+
+            // 点击取消后返回
+            if (!res.confirm) {
+                return
+            }
+            try {
+                // 调用接口删除回复
+                let deleteResponse = await api.authRequest({
+                    url: 'topics/' + topicId + '/replies/' + replyId,
+                    method: 'DELETE'
+                })
+
+                // 删除成功
+                if (deleteResponse.statusCode === 204) {
+                    wepy.showToast({
+                        title: '删除成功',
+                        icon: 'success',
+                        duration: 2000
+                    })
+                    // 将删除了的回复移除
+                    this.replies = this.replies.filter((reply) => reply.id !== replyId)
+                    this.$apply()
+                }
+
+                return deleteResponse
+            } catch (err) {
+                console.log(err)
+                wepy.showModal({
+                    title: '提示',
+                    content: '服务器错误，请联系管理员'
+                })
+            }
+        }
+    }
+
     // 获取话题回复
     async getReplies(reset = false) {
         try {
@@ -32,7 +79,10 @@ export default class ReplyMixin extends wepy.mixin {
                 let replies = repliesResponse.data.data
 
                 // 格式化回复创建时间
-                replies.forEach(function (reply) {
+                replies.forEach((reply) => {
+                    // 控制是否可以删除
+                    reply.can_delete = this.canDelete(this.user, reply)
+                    console
                     reply.created_at_diff = util.diffForHumans(reply.created_at)
                 })
                 // 如果reset不为true则合并 this.replies；否则直接覆盖
@@ -56,6 +106,22 @@ export default class ReplyMixin extends wepy.mixin {
             })
         }
     }
+    canDelete(user, reply) {
+        if (!user) {
+            console.log('User is null');
+            return false
+        }
+        console.log(reply.user_id === user.id)
+
+        return (reply.user_id === user.id)
+    }
+    async onShow() {
+        // 获取当前用户信息
+        this.user = await this.$parent.getCurrentUser()
+        console.log('get user')
+        this.$apply()
+    }
+
     async onPullDownRefresh() {
         this.noMoreData = false
         this.page = 1
